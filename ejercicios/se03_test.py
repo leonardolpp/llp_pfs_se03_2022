7e21ca25696866f09bda09744ae8c210c7fda933
leonardolpp <100705454+leonardolpp@users.noreply.github.com>
2022-03-09 09:26:59 +0100

    actualiza func primos

--- ejercicios/se03_test.py
+++ ejercicios/se03_test.py
@@ -1,3 +1,5 @@
+import math
+
 def CuentAtras(num):
     while num > 0:
         print(num)
@@ -8,7 +10,7 @@ def CuentAtrasR(num):
     # AHORA RECURSIVA
     num -= 1
     if num > 0:
-        num -= 1
+        print(num)
         CuentAtrasR(num)
         return False
     else:
@@ -24,24 +26,32 @@ def NumeroPrimo(nump):
         print('No es primo, es par: ', nump)
     else:
         i = 3
-        j = int(nump / 2)
+        # j = int(nump / 2)
+        j = int(math.sqrt(nump))
         while i <= j:
             if nump % i == 0:
-                print('*** %d NO es primo, divisible entre: %d' % (nump, i))
+                print('*** %d NO es primo, divisible entre: %d (evaluado hasta: %d)' % (nump, i, j))
                 return
             else:
                 i += 2
         print('es primo %d (%d)' % (nump, i))
 
 def NumeroPrimoR(numq, k=2):
-    # Funcion Recursiva
-    if k > (numq / 2):
+    # Funcion Recursiva --> LLEGA HASTA 992 LLAMADAS
+    # Para ampliarlo: sys.setrecursionlimit(n)
+    # if k > (numq / 2):
+    itera = int(math.sqrt(numq))
+    # if itera >= 992:
+    #     print('Func.Recursiva No lo puede calcular: %d llamadas' % itera)
+    #     return True
+    if k > itera:
         print('es primo %d (%d)' % (numq, k))
         return True
     elif numq % k == 0:
         print('%d NO es primo, divisible entre: %d' % (numq, k) )
         return False
     else:
+        # print('\t call: ', k + 1 )
         return NumeroPrimoR(numq, k + 1)
 
 if __name__ == '__main__':
@@ -54,6 +64,6 @@ if __name__ == '__main__':
     #         break
     # CuentAtras(3)
     CuentAtrasR(5)
-    X = 437
+    X = 792999901
     NumeroPrimo(X)
     NumeroPrimoR(X)
